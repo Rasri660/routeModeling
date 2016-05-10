d34405c80d72bf641485fb9756189863f0dc9e98
mickedav <mikda221@student.liu.se>
2016-05-10 23:39:05 +0200

    added DB calls to route fixer

--- dbUtil_routeFixer.py
+++ dbUtil_routeFixer.py
@@ -39,53 +39,69 @@ def getRouteStepLinks(cur, conn, route_id):
     if cur != None:
         try:
             cur.execute("""WITH test AS (SELECT *
-                                    	FROM
-                                    	(
-                                    		SELECT route_id, route_step_links.step_id, step_index, sequence_number
-                                    		FROM vm.route_step_links
-                                    		INNER JOIN vm.step_links
-                                    		ON step_links.step_id = route_step_links.step_id
-                                    		WHERE route_id = '1077:1:1'
-                                    		ORDER BY step_index, sequence_number
-                                    	) AS q1
-                                    )
+                                        	FROM
+                                        	(
+                                        		SELECT route_id, route_step_links.step_id, step_index, sequence_number
+                                        		FROM vm.route_step_links
+                                        		INNER JOIN vm.step_links
+                                        		ON step_links.step_id = route_step_links.step_id
+                                        		WHERE route_id = '1077:1:1'
+                                        		ORDER BY step_index, sequence_number
+                                        	) AS q1
+                                        )
 
-                                    SELECT q5.step_id, step_index, max,start_link, from_ref_nodeid AS start_link_start_node, to_ref_nodeid AS start_link_end_node, end_link, from_ref_nodeid AS end_link_start_node, to_ref_nodeid AS end_link_end_node
-                                    FROM
-                                    (
-                                    	SELECT q4.step_id, step_index, max,start_link, from_ref_nodeid AS start_link_start_node, to_ref_nodeid AS start_link_end_node, end_link
-                                    	FROM
-                                    	(
-                                    	SELECT q3.step_id, step_index, max, ref_lid_link as start_link ,end_link
-                                    		FROM
-                                    		(
-                                    			SELECT q2.step_id, step_index, max, ref_lid_link as end_link
-                                    			FROM
-                                    			(
-                                    				SELECT step_index, step_id, max(sequence_number)
-                                    				FROM test
-                                    				GROUP BY step_id, step_index
-                                    				ORDER BY step_index
-                                    			) AS q2
+                                        SELECT q5.step_id, step_index, max,start_link, start_link_start_node, start_link_end_node, end_link, from_ref_nodeid AS end_link_start_node, to_ref_nodeid AS end_link_end_node
+                                        --SELECT *
+                                        FROM
+                                        (
+                                        	SELECT q4.step_id, step_index, max,start_link, from_ref_nodeid AS start_link_start_node, to_ref_nodeid AS start_link_end_node, end_link
+                                        	FROM
+                                        	(
+                                        	SELECT q3.step_id, step_index, max, ref_lid_link as start_link ,end_link
+                                        		FROM
+                                        		(
+                                        			SELECT q2.step_id, step_index, max, ref_lid_link as end_link
+                                        			FROM
+                                        			(
+                                        				SELECT step_index, step_id, max(sequence_number)
+                                        				FROM test
+                                        				GROUP BY step_id, step_index
+                                        				ORDER BY step_index
+                                        			) AS q2
 
-                                    			INNER JOIN vm.step_links
-                                    			ON q2.step_id = step_links.step_id AND step_links.sequence_number = q2.max
-                                    			ORDER BY step_index
-                                    		)AS q3
-                                    		INNER JOIN vm.step_links
-                                    		ON step_links.step_id = q3.step_id
-                                    		WHERE step_links.sequence_number = 0
-                                    		ORDER BY step_index
-                                    	) AS q4
-                                    	INNER JOIN vm.ref_link_parts_network
-                                    	ON start_link = ref_link_parts_network.ref_lid
-                                    ) AS q5
+                                        			INNER JOIN vm.step_links
+                                        			ON q2.step_id = step_links.step_id AND step_links.sequence_number = q2.max
+                                        			ORDER BY step_index
+                                        		)AS q3
+                                        		INNER JOIN vm.step_links
+                                        		ON step_links.step_id = q3.step_id
+                                        		WHERE step_links.sequence_number = 0
+                                        		ORDER BY step_index
+                                        	) AS q4
+                                        	INNER JOIN vm.ref_link_parts_network
+                                        	ON start_link = ref_link_parts_network.ref_lid
+                                        ) AS q5
 
-                                    INNER JOIN vm.ref_link_parts_network
-                                    ON end_link = ref_link_parts_network.ref_lid
-                                    ORDER BY step_index""")
+                                        INNER JOIN vm.ref_link_parts_network
+                                        ON end_link = ref_link_parts_network.ref_lid
+                                        ORDER BY step_index DESC""")
         except:
             print('I cant SELECT from database')
 
         closest_source = cur.fetchall()
         return closest_source
+
+def updateStep(cur, conn, step_id, sequence_number):
+    if cur != None:
+        try:
+            cur.execute("""UPDATE vm.step_links
+                            SET link_usage = false
+                            WHERE step_id = %s AND sequence_number = %s""", [step_id, sequence_number])
+            conn.commit()
+        except:
+            print('I cant SELECT from database')
+
+
+    else:
+        print('No db connection established, try running dbConnect() first')
+        return None
--- routeFixer.py
+++ routeFixer.py
@@ -1,5 +1,7 @@
 import dbUtil_routeFixer
 
+user = 'vm-user'
+password = '001'
 
 user = "'" + user + "'"
 password = "'" + password + "'"
@@ -9,6 +11,18 @@ password = "'" + password + "'"
 #distinct_routes = dbUtil_routeFixer.getUniqueRoutes(cur, conn)
 
 data = dbUtil_routeFixer.getRouteStepLinks(cur, conn, '1077:1:1')
+number_steps = len(data)
+print(number_steps)
+for index, i in enumerate(data):
+    if index + 1 < number_steps:
 
-for i in data:
-    print(i)
+        if i[3] != i[6]:
+            prev_step = data[index + 1][0]
+            current_step = data[index][0]
+            prev_end_node = data[index + 1][8]
+            current_start_node = data[index][4]
+            sequence_number = data[index + 1][2]
+            if current_start_node != prev_end_node:
+            #print(current_step, prev_step, current_start_node, prev_end_node, sequence_number)
+                print('update:', prev_step, sequence_number)
+                dbUtil_routeFixer.updateStep(cur, conn, prev_step, sequence_number)
