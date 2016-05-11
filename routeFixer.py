026426d50c074fdb8c341a239e7f4ee84d907aa6
mickedav <mikda221@student.liu.se>
2016-05-11 10:35:41 +0200

    Revised route fixing algorithm

--- routeFixer.py
+++ routeFixer.py
@@ -16,13 +16,20 @@ print(number_steps)
 for index, i in enumerate(data):
     if index + 1 < number_steps:
 
-        if i[3] != i[6]:
-            prev_step = data[index + 1][0]
-            current_step = data[index][0]
-            prev_end_node = data[index + 1][8]
-            current_start_node = data[index][4]
-            sequence_number = data[index + 1][2]
-            if current_start_node != prev_end_node:
+        prev_step = data[index + 1][0]
+        current_step = data[index][0]
+        prev_end_link_end_node = data[index + 1][8]
+        prev_end_link_start_node = data[index + 1][7]
+        current_start_link_start_node = data[index][4]
+        prev_sequence_number = data[index + 1][2]
+        current_sequence_number = data[index][2]
+        update = 'false'
+
+        if current_start_link_start_node != prev_end_link_end_node:
+            if current_start_link_start_node == prev_end_link_start_node:
+                update = 'true'
+                print('update:', prev_step, prev_sequence_number, update)
+                dbUtil_routeFixer.updateStep(cur, conn, prev_step, prev_sequence_number)
             #print(current_step, prev_step, current_start_node, prev_end_node, sequence_number)
-                print('update:', prev_step, sequence_number)
-                dbUtil_routeFixer.updateStep(cur, conn, prev_step, sequence_number)
+
+                #dbUtil_routeFixer.updateStep(cur, conn, prev_step, sequence_number)
